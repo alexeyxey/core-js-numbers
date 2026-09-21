b7c1ccc8cf84a91eecc489175e2cf31416c85895
Ivan <vaniamar@mail.ru>
2026-09-21 21:23:03 +0700

    feat: add all solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -108,7 +108,7 @@ function getLinearEquationRoot(a, b) {
 function getAngleBetweenVectors(x1, y1, x2, y2) {
   const result = Math.acos(
     ((x1 * x2 + y1 * y2) / Math.sqrt(x1 ** 2 + y1 ** 2)) *
-    Math.sqrt(x2 ** 2 + y2 ** 2)
+      Math.sqrt(x2 ** 2 + y2 ** 2)
   );
   return result;
 }
